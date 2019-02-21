e90b557de4b4e522d90d4a33da3620dd09183acd
AnthonyMChong <Anthonymchong@gmail.com>
2019-02-21 00:10:53 -0800

    added a component! in the udemy mean tutorial section2 lecture 15 has a lot of import styling and syntax... should return later

---
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// this is a decorator
+// takes configuration via object like adding template
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+
+}
