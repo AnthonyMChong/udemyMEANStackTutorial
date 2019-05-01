67872968014a34211cc637509fc1e811db879acb
AnthonyMChong <Anthonymchong@gmail.com>
2019-04-30 23:20:48 -0700

    added a post-list component using the mat expansion panel component from angular material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 
-import { MatInputModule , MatCardModule , MatButtonModule , MatToolbarModule} from '@angular/material';
+import { MatInputModule , MatCardModule , MatButtonModule , MatToolbarModule , MatExpansionModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +26,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// this is a decorator
+// takes configuration via object like adding template
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  // styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+
+}
